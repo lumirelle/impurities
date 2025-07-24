39908b4140278d0a165f8b312ea64d4bba032f83
Lumirelle <shabbyacc@outlook.com>
2025-07-24 10:45:41 +0800

    feat: add npm script running alias of powershell & update other shell's profile

--- assets/preferences/setup-os/shell-terminal/powershell/Microsoft.PowerShell_profile.ps1
+++ assets/preferences/setup-os/shell-terminal/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,22 +1,25 @@
 # ENCODING
+# Setting the encoding to UTF-8
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [Text.Encoding]::UTF8
 
 # VARIABLES
-# -- `$env:PATH`: Add current node_modules/.bin to PATH if it exists, so you can run npm scripts without `npx`
+# $env:PATH:
+# Add current node_modules/.bin to PATH if it exists, so we can run npm scripts without `npx`
 if (Test-Path -Path "node_modules") {
   $env:PATH = "{0}{1}node_modules{1}.bin;{2}" -f (Get-Location), [System.IO.Path]::DirectorySeparatorChar, $env:PATH
 }
-# -- `$HISTORY`: Path to the PowerShell history file
+# $HISTORY: Path to the PowerShell history file
 $HISTORY = Join-Path $env:APPDATA "Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
 
-# DEV ENVIRONMENT
+# ENVIRONMENT SETUP
+# fnm
 fnm env --use-on-cd --corepack-enabled --shell powershell | Out-String | Invoke-Expression
 
 # UI
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/the-unnamed.omp.json" | Invoke-Expression
 
 # COMMANDS ENHANCEMENTS
-# -- `Clear-AllHistory`
+# Clear-AllHistory: Clear all history
 function Clear-AllHistory {
   [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
   if (Test-Path $HISTORY) {
@@ -25,12 +28,27 @@ function Clear-AllHistory {
 }
 
 # COMMAND ALIASES
-# -- `which`: Show the path of commands
+# which: Show the path of commands
 New-Alias -Name which -Value where.exe
-# -- `touch`: Create a file, using `touch` instead of `ni` (We use `ni` for `@antfu/ni`)
+# touch: Create a file, using `touch` instead of `ni` (We use `ni` for `@antfu/ni`)
 Remove-Item Alias:ni -Force -ErrorAction Ignore
 New-Alias -Name touch -Value New-Item
-# -- `grep`
+# grep
 New-Alias -Name grep -Value Select-String
-# -- `clh`: Clear All History
+
+# COMMAND SHORTCUTS
+# clh: Clear All History
 New-Alias -Name clh -Value Clear-AllHistory
+# dev, build, stub, start, release, lint, fix, typecheck:
+# Run npm scripts quickly while we are in a directory that has a `package.json`
+if (Test-Path -Path "package.json") {
+  New-Alias -Name dev -Value 'nr dev'
+  New-Alias -Name build -Value 'nr build'
+  New-Alias -Name stub -Value 'nr stub'
+  New-Alias -Name start -Value 'nr start'
+  New-Alias -Name release -Value 'nr release'
+  New-Alias -Name lint -Value 'nr lint'
+  New-Alias -Name fix -Value 'nr fix'
+  New-Alias -Name typecheck -Value 'nr typecheck'
+}
+
--- assets/preferences/setup-os/shell-terminal/zsh/.zshrc
+++ assets/preferences/setup-os/shell-terminal/zsh/.zshrc
@@ -1,6 +1,25 @@
-# run `curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.oh-my-posh` first
-eval "$(.oh-my-posh/oh-my-posh init zsh --config ~/the-unnamed.omp.json)"
-
-# manual install zsh plugin
+# Zsh plugins
 source ~/.zshplugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/.zshplugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# ENVIRONMENT SETUP
+# fnm
+eval "$(fnm env --use-on-cd --corepack-enabled --shell zsh)"
+
+# UI
+eval "$(.oh-my-posh/oh-my-posh init zsh --config ~/the-unnamed.omp.json)"
+
+# COMMAND SHORTCUTS
+# COMMAND SHORTCUTS
+# dev, build, stub, start, release, lint, fix, typecheck:
+# Run npm scripts quickly while we are in a directory that has a `package.json`
+if [ -f package.json ]; then
+  alias dev='nr dev'
+  alias build='nr build'
+  alias stub='nr stub'
+  alias start='nr start'
+  alias release='nr release'
+  alias lint='nr lint'
+  alias fix='nr fix'
+  alias typecheck='nr typecheck'
+fi
