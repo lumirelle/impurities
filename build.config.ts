5ef1a71cbbad2bea56f45e5e0349ba0e1db9d10a
Lumirelle <shabbyacc@outlook.com>
2025-07-30 13:44:20 +0800

    fix: :bug: missing entries on build config

--- build.config.ts
+++ build.config.ts
@@ -1,15 +1,10 @@
-import { basename } from 'node:path'
-import { globSync } from 'tinyglobby'
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-  entries: globSync(
-    ['src/commands/*.ts'],
-    { expandDirectories: false },
-  ).map(i => ({
-    input: i.slice(0, -3),
-    name: basename(i).slice(0, -3),
-  })),
+  entries: [
+    'src/index.ts',
+    'src/cli.ts',
+  ],
   clean: true,
   declaration: 'node16',
   rollup: {
