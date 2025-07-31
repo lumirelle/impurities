4682e1a4e30de3990321bb8ef793c9a477fc68c4
Lumirelle <shabbyacc@outlook.com>
2025-07-31 12:02:20 +0800

    feat: :sparkles: update commitlint config

--- assets/preferences/setup-project/common/commitlint.config.js
+++ assets/preferences/setup-project/common/commitlint.config.js
@@ -4,12 +4,4 @@
  */
 export default {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    // <type>, based on the conventional commit spec, add 'merge' and 'wip' to the list.
-    'type-enum': [
-      2,
-      'always',
-      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'merge', 'wip'],
-    ],
-  },
 }
--- assets/preferences/setup-project/common/commitlint.config.ts
+++ assets/preferences/setup-project/common/commitlint.config.ts
@@ -5,14 +5,6 @@ import type { UserConfig } from '@commitlint/types'
  */
 const config: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    // <type>, based on the conventional commit spec, add 'merge' and 'wip' to the list.
-    'type-enum': [
-      2,
-      'always',
-      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'merge', 'wip'],
-    ],
-  },
 }
 
 export default config
