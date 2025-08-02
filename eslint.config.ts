b4d6b11db8223ecebfc7ba1da328d2270bb2c9bc
Lumirelle <shabbyacc@outlook.com>
2025-08-02 13:29:27 +0800

    chore: :hammer: exclude tests in root eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,9 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   pnpm: true,
   formatters: true,
+  ignores: [
+    'tests/**',
+  ],
 })
   .override('antfu/yaml/pnpm-workspace', {
     rules: {
